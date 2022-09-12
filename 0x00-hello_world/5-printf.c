14b5e5e5f3c52e6341971139168849e7766d21e3
Ozi <Kelli@example.com>
2022-09-11 17:14:44 -0700

    task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,9 +3,9 @@
  * main-entry point
  * Return: 0
  */
-	int main(void) 
-{
-	printf("with proper grammer, but the outcome is a peice of art,\n");
+int main(void)
 
+{	printf("with proper grammar, but the outcome is a piece of art,\n");
+	
 	return (0);
 }
