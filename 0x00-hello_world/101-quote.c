ebec6bfec464c0dea6645f212e764be7484b6974
Ozi <Kelli@example.com>
2022-09-18 11:59:36 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,14 +3,9 @@
 #include <unistd.h>
 
 /**
- *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 
