c2e197ae117c4e0ac9d141a5adb4271225a00f0f
Ozi <Kelli@example.com>
2022-09-19 22:50:29 -0700

    3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -8,12 +8,11 @@
 
 void _puts(char *str)
 {
-	int i = 0;
+	int i;
 
-	while (*(str + i) != '\0')
+	for (i = 0; str[i] != '\0'; i++)
 	{
-		putchar(*(str + i));
-		i++;
+		_putchar(str[i]);
 	}
-	putchar(10);
+	_putchar('\n');
 }
